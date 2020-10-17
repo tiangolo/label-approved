1585976bfc987a4be608f975a18e0643c66d86d8
Sebastián Ramírez <tiangolo@gmail.com>
2020-10-17 13:04:29 +0200

    🐛 Fix logic to compute the number of approvals, use last per user

--- app/__main__.py
+++ app/__main__.py
@@ -2,6 +2,7 @@ import logging
 from typing import Dict, Optional
 
 from github import Github
+from github.PullRequestReview import PullRequestReview
 from pydantic import BaseSettings, SecretStr
 from pydantic.main import BaseModel
 
@@ -33,13 +34,19 @@ for pr in repo.get_pulls(state="open"):
     pr_labels = list(pr.get_labels())
     pr_label_by_name = {label.name: label for label in pr_labels}
     reviews = list(pr.get_reviews())
+    review_by_user: Dict[str, PullRequestReview] = {}
+    for review in reviews:
+        if review.user.login in review_by_user:
+            stored_review = review_by_user[review.user.login]
+            if review.submitted_at >= stored_review.submitted_at:
+                review_by_user[review.user.login] = review
+    approved_reviews = [
+        review for review in review_by_user.values() if review.state == "APPROVED"
+    ]
     for approved_label, conf in settings.input_config.items():
         logging.debug(f"Processing config: {conf.json()}")
         if conf.await_label is None or (conf.await_label in pr_label_by_name):
             logging.debug(f"Processable PR: {pr.number}")
-            approved_reviews = [
-                review for review in reviews if review.state == "APPROVED"
-            ]
             if len(approved_reviews) >= conf.number:
                 logging.info(f"Adding label to PR: {pr.number}")
                 pr.add_to_labels(approved_label)
